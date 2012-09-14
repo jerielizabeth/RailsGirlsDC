3162a4ada7c1c5e4a13d8a65e6fc0bdc2fd331af
Jeri Wieringa <jeri.elizabeth@gmail.com>
2012-09-14 19:09:22 -0400

    Initial setup for railsgirls
    Not sure this will be the actual project, but it's a start.

---
+++ twitterscraper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitterscraper::Application.config.secret_token = '7cd52ff6158e4cebd8d4b6c3cb7bb7ec25dc3a2eb03b3e8829f08746d3d71a46e8e20c93fd2231609e7c39303aa77fa27c20aa2efd98c8683496a511f3ed2ea7'
